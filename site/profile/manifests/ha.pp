ab2fd7dc726bd982ed4409c112ee61b4f48dbdc4
pipeiden <felipesantibanezj@gmail.com>
2020-08-23 20:59:28 -0400

    Update ha.pp

--- site/profile/manifests/ha.pp
+++ site/profile/manifests/ha.pp
@@ -1,4 +1,13 @@
 class profile:ha {
   include docker
+  docker::image { 'homeassistant/home-assistant':
+    ensure => present,
+    image_tag => '0.114.3'      
+  }
+  
+  docker::run { 'homeassistant/home-assistant':
+    image => 'homeassistant/home-assistant:0.114.3'
+    ports => ['80:8123']
+  }
 }
 
